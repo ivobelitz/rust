e99109ac0bdb6f6e38efe56e6de9127bc6d0cbd8
Ivo Belitts <belitzivo@gmail.com>
2021-11-01 18:12:33 +0100

    Enums

---
+++ 6.enums/target/rls/debug/deps/enums-184ed9b11455c198.d
@@ -0,0 +1,5 @@
+/home/ivo/Rust/6.enums/target/rls/debug/deps/enums-184ed9b11455c198.rmeta: src/main.rs
+
+/home/ivo/Rust/6.enums/target/rls/debug/deps/enums-184ed9b11455c198.d: src/main.rs
+
+src/main.rs:
---
+++ 6.enums/target/rls/debug/deps/enums-d90f2549b8e4b174.d
@@ -0,0 +1,5 @@
+/home/ivo/Rust/6.enums/target/rls/debug/deps/enums-d90f2549b8e4b174.rmeta: src/main.rs
+
+/home/ivo/Rust/6.enums/target/rls/debug/deps/enums-d90f2549b8e4b174.d: src/main.rs
+
+src/main.rs:
